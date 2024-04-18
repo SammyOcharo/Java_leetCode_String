7b957066fd97de6824b61fb35f7acce3f390ab60
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-04-18 08:44:25 +0300

    leet code problem(change roman string to integer JUnit test cases)

--- src/test/java/com/sam/leetCodeString/RomanToIntegerTest.java
+++ src/test/java/com/sam/leetCodeString/RomanToIntegerTest.java
@@ -10,5 +10,7 @@ class RomanToIntegerTest {
     void romanToInt() {
         assertEquals(3, RomanToInteger.romanToInt("III"));
         assertEquals(1994, RomanToInteger.romanToInt("MCMXCIV"));
+        assertEquals(888, RomanToInteger.romanToInt("DCCCLXXXVIII"));
+        assertNotEquals(1993, RomanToInteger.romanToInt("MCMXCIV"));
     }
 }
