5d1271f08d049bec9f37c94e2e07233a9f46625e
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-04-27 21:59:23 +0300

    leet code string problem(find final value after operation JUnit tests)

--- src/test/java/com/sam/leetCodeString/FinalValueOfStringTest.java
+++ src/test/java/com/sam/leetCodeString/FinalValueOfStringTest.java
@@ -11,6 +11,9 @@ class FinalValueOfStringTest {
     @Test
     void finalValueAfterOperations() {
         String[] arr = {"--X","X++","X++"};
+        String[] arr1 = {"X++","++X","--X","X--"};
         assertEquals(1, FinalValueOfString.finalValueAfterOperations(arr));
+        assertEquals(0, FinalValueOfString.finalValueAfterOperations(arr1));
+        assertNotEquals(0, FinalValueOfString.finalValueAfterOperations(arr));
     }
 }
