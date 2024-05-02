7bef2a5687c772b51a8df08440eff1dcb0998899
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-05-02 07:38:07 +0300

    leetcode problem of the day(String array equivalent JUnit test cases)

--- src/test/java/com/sam/leetCodeString/StringArraysEquivalentTest.java
+++ src/test/java/com/sam/leetCodeString/StringArraysEquivalentTest.java
@@ -10,6 +10,15 @@ class StringArraysEquivalentTest {
     void arrayStringsAreEqual() {
         String[] word1 = {"ab", "c"};
         String[] word2 = {"a", "bc"};
+
+        String[] word3 = {"abc", "d", "defg"};
+        String[] word4 = {"abcddefg"};
+
+        String[] word5 = {"a", "cb"};
+        String[] word6 = {"ab", "c"};
+
         assertTrue(StringArraysEquivalent.arrayStringsAreEqual(word1, word2));
+        assertTrue(StringArraysEquivalent.arrayStringsAreEqual(word3, word4));
+        assertFalse(StringArraysEquivalent.arrayStringsAreEqual(word5, word6));
     }
 }
