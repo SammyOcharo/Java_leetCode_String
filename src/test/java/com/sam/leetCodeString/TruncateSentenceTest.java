ebf3dc38d559c55f3a07ed22f7bf76527346dc60
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-05-07 07:50:20 +0300

    leetcode problem(truncate sentence J Unit tests)

--- src/test/java/com/sam/leetCodeString/TruncateSentenceTest.java
+++ src/test/java/com/sam/leetCodeString/TruncateSentenceTest.java
@@ -9,5 +9,8 @@ class TruncateSentenceTest {
     @Test
     void truncateSentence() {
         assertEquals("Hello how are you", TruncateSentence.truncateSentence("Hello how are you Contestant",4));
+        assertEquals("What is the solution", TruncateSentence.truncateSentence("What is the solution to this problem", 4));
+        assertEquals("chopper is not a tanuki", TruncateSentence.truncateSentence("chopper is not a tanuki", 5));
+        assertNotEquals("chopper is not tanuki", TruncateSentence.truncateSentence("chopper is not a tanuki", 5));
     }
 }
