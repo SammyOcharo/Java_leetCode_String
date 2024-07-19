c679b06f01cb6ea2ba890cdb77722d82c297b270
sammy ocharo <63811481+SammyOcharo@users.noreply.github.com>
2024-07-19 23:18:29 +0300

    Update LongestCommonPrefix.java

--- src/main/java/com/sam/leetCodeString/LongestCommonPrefix.java
+++ src/main/java/com/sam/leetCodeString/LongestCommonPrefix.java
@@ -7,11 +7,10 @@ public class LongestCommonPrefix {
     //If there is no common prefix, return an empty string "".
 
     //Example 1:
-    //
     //Input: strs = ["flower","flow","flight"]
     //Output: "fl"
     //Example 2:
-    //
+
     //Input: strs = ["dog","racecar","car"]
     //Output: ""
     //Explanation: There is no common prefix among the input strings.
